79e3d597fa0bfdf17855c9db049c2b6bb86e3e7d
Antonio Le <136533712+Antuuna@users.noreply.github.com>
2024-02-13 11:45:16 -0800

    Created initial files

---
+++ pokemon.h
@@ -0,0 +1,8 @@
+#ifndef POKEMON_H
+#define POKEMON_H
+#include <iostream>
+
+class Pokemon {
+};
+
+#endif
---
+++ pokemonMain.cpp
@@ -0,0 +1,8 @@
+#include "pokemon.h"
+
+using namespace std;
+
+int main() {
+
+  return 0;
+}
